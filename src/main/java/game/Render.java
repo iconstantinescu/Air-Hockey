0c11df905b452462a6c2324caf30ef2c28006a0f
Jaron Rosenberg <j.m.rosenberg@student.tudelft.nl>
2019-12-05 16:19:14 +0100

    Add the rendermenu to the new project style

--- src/main/java/game/Render.java
+++ src/main/java/game/Render.java
@@ -5,10 +5,11 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import menu.RenderMenu;
 
 public class Render extends ApplicationAdapter {
 
-    enum GameState {
+    public enum GameState {
         LOGIN,
         MENU,
         GAME
@@ -16,50 +17,15 @@ public class Render extends ApplicationAdapter {
 
     protected static GameState gameState;
     private static Renderer renderer;
-    private static SpriteBatch homeBatch;
-    private static SpriteBatch playBatch;
-    private static SpriteBatch scoresBatch;
-    private static SpriteBatch quitBatch;
-
-    private static Texture home;
-    private static Texture play;
-    private static Texture scores;
-    private static Texture quit;
-
-    private static Sprite homeSprite;
-    private static Sprite playSprite;
-    private static Sprite scoresSprite;
-    private static Sprite quitSprite;
 
     @Override
     public void create() {
 
 
-        homeBatch = new SpriteBatch();
-        playBatch = new SpriteBatch();
-        scoresBatch = new SpriteBatch();
-        quitBatch = new SpriteBatch();
-
-        home = new Texture("home.png");
-        homeSprite = new Sprite(home);
-        homeSprite.setPosition(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
-        play = new Texture("play.png");
-        playSprite = new Sprite(play);
-        playSprite.setPosition(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
-        playSprite.setPosition(playSprite.getX(),
-                playSprite.getY() + playSprite.getHeight() / 2);
-        scores = new Texture("scores.png");
-        scoresSprite = new Sprite(scores);
-        scoresSprite.setPosition(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
-        quit = new Texture("quit.png");
-        quitSprite = new Sprite(quit);
-        quitSprite.setPosition(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
-        quitSprite.setPosition(quitSprite.getX(),
-                quitSprite.getY() - quitSprite.getHeight() / 2);
 
         gameState = GameState.GAME;
 
-        renderer = new RenderGame();
+        renderer = new RenderMenu();
 
     }
 
@@ -72,12 +38,13 @@ public class Render extends ApplicationAdapter {
      * Change Game State.
      * @param newGameState The New Game State
      */
-    public void changeGameState(GameState newGameState) {
+    public static void changeGameState(GameState newGameState) {
         gameState = newGameState;
         switch (gameState) {
             case LOGIN:
                 break;
             case MENU:
+                renderer = new RenderMenu();
                 break;
             case GAME:
                 renderer = new RenderGame();
@@ -94,14 +61,7 @@ public class Render extends ApplicationAdapter {
         if (renderer != null) {
             renderer.dispose();
         }
-        homeBatch.dispose();
-        playBatch.dispose();
-        scoresBatch.dispose();
-        quitBatch.dispose();
-        home.dispose();
-        play.dispose();
-        scores.dispose();
-        quit.dispose();
+
         //                img.dispose();
         //                shape.dispose();
         //                hitSound.dispose();
