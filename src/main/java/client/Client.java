0652cf5eeca1ea5426954de9d963b09c0f3fc1ca
Jaron Rosenberg <j.m.rosenberg@student.tudelft.nl>
2019-12-17 16:15:37 +0100

    Fix the pmd and checkstyle errors

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,31 +1,31 @@
 package client;
 
-import game.Render;
-
 public class Client {
 
     /**
      * Authenticates username and password.
+     *
      * @param passInput inputted password.
      * @param nameInput inputted username.
      * @return if combination is found in the database.
      */
     public boolean authenticate(String passInput, String nameInput) {
-        AuthenticationController authenticationController =
-                new AuthenticationController(new ConnectionFactory());
-        if(passInput.equals("") || nameInput.equals("")){
+        if (passInput.equals("") || nameInput.equals("")) {
             System.out.println("empty field");
             return false;
         }
+        AuthenticationController authenticationController =
+                new AuthenticationController(new ConnectionFactory());
         String salt = authenticationController.getSalt(nameInput);
         return authenticationController.authenticate(nameInput, passInput, salt);
     }
 
     /**
      * registers user in database.
+     *
      * @param passInput inputted password.
      * @param nameInput inputted name.
-     * @param nickname inputted nickname.
+     * @param nickname  inputted nickname.
      */
     public void register(String nameInput, String passInput, String nickname) {
         RegistrationController registrationController =
