9a98456bc631f91193b2ee994424757058030bf1
Darwin <d.b.morris@student.tudelft.nl>
2019-12-17 15:56:35 +0100

    Updated: changed layout of login

--- src/main/java/menu/RenderLogin.java
+++ src/main/java/menu/RenderLogin.java
@@ -9,9 +9,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
@@ -26,8 +24,8 @@ public class RenderLogin implements Renderer {
     transient Stage stage;
     transient TextButton loginButton;
     transient TextButton registerButton;
-    transient TextField passwordText;
-    transient TextField usernameText;
+    transient Label passwordText;
+    transient Label usernameText;
     transient Skin skin;
 
     transient String nameInput;
@@ -40,35 +38,39 @@ public class RenderLogin implements Renderer {
 
         skin = new Skin(Gdx.files.internal("assets/ui/skin/uiskin.json"));
         stage = new Stage(new ScreenViewport());
+        float centerW = Gdx.graphics.getWidth() / 2;
+        float centerH = Gdx.graphics.getHeight() / 2;
+
+        username = new TextField("", skin);
+        username.setSize(300, 40);
+        float usernameX = centerW - username.getWidth() / 2;
+        float usernameY = centerH + username.getHeight();
+        username.setPosition(usernameX,usernameY);
+
+        password = new TextField("", skin);
+        password.setSize(300,40);
+        password.setPosition(usernameX, usernameY - username.getHeight() - 5);
+        password.setPasswordMode(true);
+        password.setPasswordCharacter('*');
 
         loginButton = new TextButton("login",skin,"default");
         loginButton.setWidth(200);
         loginButton.setHeight(40);
-        loginButton.setPosition(300,150);
+        loginButton.setPosition(usernameX,usernameY - username.getHeight() * 2 - 5);
 
         registerButton = new TextButton("register", skin, "default");
         registerButton.setWidth(200);
         registerButton.setHeight(40);
-        registerButton.setPosition(510, 150);
+        registerButton.setPosition(usernameX + 250, usernameY - username.getHeight() * 2 - 5);
 
-        username = new TextField("", skin);
-        username.setPosition(300,250);
-        username.setSize(300, 40);
-
-        password = new TextField("", skin);
-        password.setPosition(300, 200);
-        password.setSize(300,40);
-        password.setPasswordMode(true);
-        password.setPasswordCharacter('*');
-
-        usernameText = new TextField("username:", skin);
+        usernameText = new Label("username:", skin);
         usernameText.setSize(100, 40);
-        usernameText.setPosition(190, 250);
+        usernameText.setPosition(usernameX - 110, usernameY);
 
 
-        passwordText = new TextField("password:", skin);
+        passwordText = new Label("password:", skin);
         passwordText.setSize(100, 40);
-        passwordText.setPosition(190, 200);
+        passwordText.setPosition(usernameX - 110, usernameY - username.getHeight() - 5);
 
         loginButton.addListener(new ClickListener() {
             @Override
